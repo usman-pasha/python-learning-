2332c387129ae53ef32023e779444acc88d1b099
usman.backend.dev <usman@asztechnologies.com>
2024-06-14 14:33:36 +0530

    variable

---
+++ 01Variables/02VariableName.py
@@ -0,0 +1,42 @@
+# Variable Names
+
+# A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). Rules for Python variables:
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+# A variable name cannot be any of the Python keywords.
+
+# 1. Legal variable names:
+myvar = "usman pasha"
+my_var = "usman pasha"
+_my_var = "usman pasha"
+myVar = "usman pasha"
+MYVAR = "usman pasha"
+myvar2 = "usman pasha"
+
+# 2. Illegal variable names:
+
+# 2myvar = "usman pasha"
+# my-var = "usman pasha"
+# my var = "usman pasha"
+
+# Multi Words Variable Names
+# Variable names with more than one word can be difficult to read.
+
+# There are several techniques you can use to make them more readable:
+
+# 1. Camel Case
+# Each word, except the first, starts with a capital letter:
+
+myVariableName = "Mohammed Siraj"
+
+# 2. Pascal Case
+# Each word starts with a capital letter:
+
+MyVariableName = "Mohammed Siraj"
+
+# 3. Snake Case
+# Each word is separated by an underscore character:
+
+my_variable_name = "Mohammed Siraj"
